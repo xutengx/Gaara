9cf6312a8c41fbe1f1bd6c15b354b1e220fed16d
xT <1771033392@qq.com>
2016-02-25 18:14:45 +0800

    删除目录project

--- project/Main/Exception.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace Main;
-class Exception extends \Exception{
-
-}
--- project/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-defined('IN_SYS')|| define('IN_SYS', substr(str_replace('\\','/',__FILE__),strrpos(str_replace('\\','/',__FILE__),'/')+1));
-require './init.php';

a55fcf4855d9cfaafbd0ef0cf654a75a9bbb4115
xT <1771033392@qq.com>
2016-02-25 16:10:46 +0800

    the first times

---
+++ Main/conf.class.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/12/21 0021
+ * Time: 17:12
+ */
+namespace Main;
+defined('IN_SYS')||exit('ACC Denied');
+class conf{
+    protected static $ins = null;
+    protected $data = array();
+
+    final protected function __construct(){
+        include   (ROOT.'config.inc.php');
+        $this->data = $_CFG;						//配置文件信息,读过来,赋给data属性
+        $this->makeDefine();
+        $this->set();
+    }
+    final protected function __clone(){				//反克隆
+        exit();
+    }
+    public static function getins(){
+        if((self::$ins instanceof self) || (self::$ins = new self()))  return self::$ins;
+    }
+    public function __get($key){					//用魔术方法,读取data内的信息
+        if(array_key_exists($key, $this->data)) return $this->data[$key];
+        else return null;
+    }
+    public function __set($key,$value){			// 用魔术方法,在运行期,动态增加或改变配置选项
+        $this->data[$key] = $value ;
+    }
+    private function makeDefine(){
+        define('PATH', $this->data['path']);
+        define('SESSIONPATH', $this->data['sessionPath']);
+        define('SESSIONLIFE', $this->data['sessionLife']);
+        define('APPID', $this->data['appid']);
+        define('APPSECRET', $this->data['appsecret']);
+    }
+    private function set(){
+        date_default_timezone_set($this->data['timezone']);
+    }
+}
---
+++ Main/func.class.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * 柯里化,多态
+ * User: Administrator
+ * Date: 2016/2/4 0004
+ * Time: 10:54
+ */
+/**
+ * 实例化对象,包含自动加载
+ * 依赖 loader.class.php 指向 obj::get()
+ * @param $class
+ * @param bool|true $app 当$class为Contr或Module时,代表所指向的APP,默认当前APP;
+ *                        当$class为其他引用类时,其他参数生效,$app默认为true,代表单例模式实例化;
+ * @param 其他参数, 在new非Contr或Module对象时的参数, 注:单例模式下,显然只有第一次实例化时,参数才会被使用!
+ * @return mixed 对象
+ */
+function obj($obj, $app=true){
+    $arr = func_get_args();
+    unset($arr[0]);
+    unset($arr[1]);
+    if(empty($arr)) return \Main\obj::get($obj, $app);
+    else return \Main\obj::get($obj, $app, $arr);
+}
+
+/**
+ * 依赖 template.class.php 指向 template::show()
+ * @param string $template 引入模板名
+ */
+function template($template=''){
+    if($template) \Main\template::show($template);
+    else throw new \Exception('引入模板名有误!');
+}
+
+/**
+ * 发送HTTP状态
+ * @param integer $code 状态码
+ * @return void
+ */
+function send_http_status($code) {
+    static $_status = array(
+        // Informational 1xx
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        // Success 2xx
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        // Redirection 3xx
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Moved Temporarily ',  // 1.1
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        // 306 is deprecated but reserved
+        307 => 'Temporary Redirect',
+        // Client Error 4xx
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        // Server Error 5xx
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        509 => 'Bandwidth Limit Exceeded'
+    );
+    if(isset($_status[$code])) {
+        header('HTTP/1.1 '.$code.' '.$_status[$code]);
+        // 确保FastCGI模式下正常
+        header('Status:'.$code.' '.$_status[$code]);
+    }
+}

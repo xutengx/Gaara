28421c0c4da9c5b041e94ff7795c5a6af236268d
xT <1771033392@qq.com>
2016-03-16 11:19:26 +0800

    很多的更改.. 1.\Expand\Image 中添加压缩图片方法,没有优化但是可以用用; 2.\Expand\Wechat 文件Boom头问题移除; 3.\Main\Module 添加tbname()返回当前数据表名,可以在join中配合 obj('userModule')->tbname()使用; 4.\Main\Controller 添加arrayPost()将$_POST数据提取,用于大表单提交,属于post()的封装方法; 5.删除部分示例文件 6.还有其他的..但不重要

---
+++ project/Application/index/Controller/indexContr.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace index;
+defined('IN_SYS')||exit('ACC Denied');
+class indexContr extends \Main\Controller{
+    public function indexDo(){
+        echo '<br/>bigin<br/>';
+        $i = 100;
+    }
+    public function test(){
+
+    }
+}

31f6e679a33715c6550ebe63759c73df2664cc4f
xT <1771033392@qq.com>
2017-08-01 18:54:09 +0800

    test

--- project/Route/http.php
+++ project/Route/http.php
@@ -6,10 +6,24 @@ return [
     },
             
             
+    '/test/{id?}' => [
+        'domain' => '{username}.gitxt.com',
+        'as' => '别名dis',
+        'uses'=> function($id){
+            
+            var_dump($id);
+        
+            var_dump($_SERVER);exit;
+        }
+    ],
     '/index.php/test/user/{id?}' => 'App\index\Contr\IndexContr@indexDo',
     '/mysql' => 'App\mysql\Contr\indexContr@indexDo',
     '/power.html' => 'App\jurisdiction\Contr\IndexContr@index',
-    '/enable.html' => ['as' => '别名dis', 'uses'=> 'App\jurisdiction\Contr\enable@index'],
+    '/enable.html' => [
+        'domain' => '{username}.gitxt.com',
+        'as' => '别名dis',
+        'uses'=> 'App\jurisdiction\Contr\enable@index'
+    ],
     '/disable.html' => 'App\jurisdiction\Contr\disable@index'
 
 ];

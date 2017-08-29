fdf118e7edde69f679adb8482342497ad901b043
xT <1771033392@qq.com>
2017-08-29 11:07:58 +0800

    目录调整

---
+++ Main/Core/Object.php
@@ -0,0 +1,55 @@
+<?php
+namespace Main\Core;
+defined('IN_SYS')||exit('ACC Denied');
+abstract class Object{
+    // 对象属性
+    protected $obj_attr = array();
+    // 绑定模型
+    protected $obj_module = null;
+
+    final public function __construct(){
+        $this->construct();
+    }
+    protected function construct(){
+
+    }
+    /**
+     * 绑定数据库模型到obj
+     * @param object $obj 如 obj('userModule')
+     * @return $this
+     */
+    final public function init($obj){
+        $this->obj_module = $obj;
+        if(!$this->obj_attr = $this->bind())
+            $this->bindFalse();
+        return $this;
+    }
+
+    /**
+     * 绑定 model 到 obj
+     * @return array|false 一维数组
+     */
+    abstract protected function bind();
+
+    /**
+     * bind() 失败(returne false)之后操作
+     * @return mixed
+     */
+    abstract protected function bindFalse();
+
+    /**
+     * 更新当前对象属性到绑定的数据库模型
+     * @return bool|int
+     */
+    final public function save(){
+        return $this->obj_module->data($this->obj_attr)->update();
+    }
+    final public function __set($property_name, $value){
+        $this->obj_attr[$property_name] = $value;
+    }
+    final public function __get($property_name){
+        if(isset($this->obj_attr[$property_name]))
+            return $this->obj_attr[$property_name];
+        else throw new Exception('不存在的属性!');
+    }
+}
--- project/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-//phpinfo();exit;
-// 性能更加~!
-//var_dump($_SERVER);exit;
-defined('IN_SYS')|| define('IN_SYS', substr(str_replace('\\','/',__FILE__),strrpos(str_replace('\\','/',__FILE__),'/')+1));
-//defined('IN_SYS')|| define('IN_SYS', basename(realpath(__FILE__)));
-require './init.php';
-

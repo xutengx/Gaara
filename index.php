b96f58952b30bc3143f698b8ec2971075ac309c9
xT <1771033392@qq.com>
2017-08-29 11:30:14 +0800

    提交

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
-//phpinfo();exit;
-// 性能更加~!
-//var_dump($_SERVER);exit;
+/*
+|--------------------------------------------------------------------------
+| 入口文件,IN_SYS 可以被用来做一些有趣的事情
+|--------------------------------------------------------------------------
+| init.php 作为初始化
+|
+*/
 defined('IN_SYS')|| define('IN_SYS', substr(str_replace('\\','/',__FILE__),strrpos(str_replace('\\','/',__FILE__),'/')+1));
-//defined('IN_SYS')|| define('IN_SYS', basename(realpath(__FILE__)));
+
 require './init.php';
 

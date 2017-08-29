cf09b414fbbfde1350aed8a2f82b3109846d1951
xT <1771033392@qq.com>
2017-08-29 17:02:51 +0800

    备注

--- getInfoOnWechat.php
+++ getInfoOnWechat.php
@@ -1,5 +1,6 @@
 <?php
 // 微信授权,专用入口
+// test
 define('IN_SYS', substr(str_replace('\\','/',__FILE__),strrpos(str_replace('\\','/',__FILE__),'/')+1));
 if(  (!isset($_GET[MD5(IN_SYS)])||empty($_GET[MD5(IN_SYS)])) && isset($_GET['code']) && !empty($_GET['code'])){
     $_GET[MD5(IN_SYS)] = 'index/login/getInfoOnWechatProfessional/';

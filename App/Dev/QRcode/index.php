0e55827a4c7ec73ca3c3a6dba1cc211fe46fb499
xuteng <68822684@qq.com>
2018-01-22 16:07:25 +0800

    更新 chillerlan/php-qrcode thanks Smiley

--- App/Dev/QRcode/index.php
+++ App/Dev/QRcode/index.php
@@ -8,6 +8,7 @@ class index{
     public function index(\QRcode $QRcode){
         $QRcode->pixelSize = 11;
 //        $QRcode->cachefile = ROOT.'data/Cache/qwewqewqewq.png';
+//		var_dump(\QRcode::base64('https://www.baidu.com'));exit;
         echo '<img src="'.\QRcode::base64('https://www.baidu.com').'" />';
         exit;
     }
